278164146199609feaf9f224b128b0ce1366281f
Tim Holeski <trholeski@gmail.com>
2019-09-09 19:43:00 -0700

    created logic to compute best friend, built modal to display best friend, and added the rest of survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,9 +2,13 @@ module.exports.friends = [
     {
         routeName: "",
         name: "bob",
-        email: "sdf@sdf.com",
-        passsword: "asdasda",
+        pic: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.enjoyillinois.com%2Fassets%2FTourism-Operators%2Fimages%2Fhardys-reindeer-face.jpg&f=1&nofb=1",
         traits: [
+            "1",
+            "2",
+            "4",
+            "2",
+            "1",
             "1",
             "2",
             "4",
@@ -15,26 +19,34 @@ module.exports.friends = [
     {
         routeName: "",
         name: "joe",
-        email: "sdf@sdf.com",
-        passsword: "asdasda",
+        pic: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F96%2Fd1%2Fd6%2F96d1d63d647266d829670db73e92b2f4--choose-to-be-happy-mobile-phones.jpg&f=1&nofb=1",
         traits: [
             "2",
             "1",
             "2",
             "1",
-            "2"
+            "2",
+            "1",
+            "2",
+            "4",
+            "2",
+            "1"
         ]
     },
     {
         routeName: "",
         name: "billy",
-        email: "sdf@sdf.com",
-        passsword: "asdasda",
+        pic: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.umt.edu%2Fweb%2Fplugins%2Fgalleriffic%2Fdemo_images%2Fbear_face.jpg&f=1&nofb=1",
         traits: [
             "1",
             "1",
             "4",
             "1",
+            "1",
+            "1",
+            "2",
+            "4",
+            "2",
             "1"
         ]
     },
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,30 +11,21 @@ module.exports = function(app) {
 // posting data to the api
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to a user"s survey result
-    // Then compare those results against every user in the database.
-    // It will then calculate the difference between each of the numbers and the user"s numbers.
-    // It will then choose the user with the least differences as the "best friend match."
-    // In the case of multiple users with the same result it will choose the first match.
-    // After the test, it will push the user to the database.
 
-    // We will use this object to hold the "best match". We will constantly update it as we
-    // loop through all of the options
     var bestFriend = {
       name: "",
-      photo: "",
+      pic: "",
       friendDifference: Infinity
     };
 
-    // Here we take the result of the user"s survey POST and parse it.
+    //user data being stored in a var
     var newFriend = req.body;
     var newFriendTraits = newFriend.traits;
 
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
+    // variation between user and potential best friends
     var totalDifference;
 
-    // Here we loop through all the friend possibilities in the database.
+    // Loop possibilities currently stored
     for (var i = 0; i < friendsFile.friends.length; i++) {
       var currentFriend = friendsFile.friends[i];
       totalDifference = 0;
@@ -50,22 +41,20 @@ module.exports = function(app) {
         totalDifference += Math.abs(parseInt(currentUserTraits) - parseInt(currentFriendTraits));
       }
 
-      // If the sum of differences is less then the differences of the current "best match"
+      //logic used to determine whether new match is better than the last match.  if it is better, then the current friend replaces the existing "best friend"
       if (totalDifference <= bestFriend.friendDifference) {
-        // Reset the bestMatch to be the new friend.
         bestFriend.name = currentFriend.name;
-        bestFriend.photo = currentFriend.photo;
+        bestFriend.pic = currentFriend.pic;
         bestFriend.friendDifference = totalDifference;
       }
     }
-
+    //dislay the best friend for error check.
     console.log("your best friend is: ", bestFriend);
 
-    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-    // the database will always return that the user is the user's best friend).
+    // save user's data to the database
     friendsFile.friends.push(newFriend);
 
-    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
-    res.json(friendsFile.friends);
+    // return list of friends json to close the request
+    res.json(bestFriend);
   });
 };
